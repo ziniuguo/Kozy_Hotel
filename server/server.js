9d7ec2eff59f98c339b8306f53a68a0ec222e7d9
Ziniu <guo.ziniu.1003@gmail.com>
2022-07-12 14:55:53 +0800

    search-backend

--- server/server.js
+++ server/server.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express()
 
+// need to add session management. 
+const myJSON = {"hotels": ["hotel one", "hotel two", "hotel two2"]};
+
+function search() {
+
+}
+
 app.get("/api", (req, res) => {
-    res.json({"hotels": ["hotel 1", "hotel 2", "hotel 3"]})
+    res.json(myJSON);
 })
+
 
 app.get("/searchapi", (req, res) => {
     // sample response. Someone do the search function here
-    res.json(req.query)
+    const test = req.query;
+    console.log(req.query.q);
+    if (req.query.q !== 'undefined') {
+        let result = {"hotels": []};
+        let keyword = req.query.q;
+        for (let i = 0; i < myJSON.hotels.length; i++) {
+            console.log(myJSON.hotels[i])
+            if (myJSON.hotels[i].toUpperCase().includes(keyword.toUpperCase())) {
+                result.hotels.push(myJSON.hotels[i]);
+                console.log(myJSON.hotels[i] + " added");
+            }
+        }
+        console.log(result);
+        res.json(result);
+    } else {
+        res.json(test);
+    }
 })
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.listen(5000, () => {
+    console.log("Server started on port 5000")
+})
