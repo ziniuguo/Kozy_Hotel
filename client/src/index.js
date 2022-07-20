bff9ec7f1053498c9521be1ee63fc6f1615c8be6
lonelyhat <avalonglory@gmail.com>
2022-07-20 17:29:15 +0800

    update

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 // import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
