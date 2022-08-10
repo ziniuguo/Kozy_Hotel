2721df29a78ae99c2998e18d4d384646a47d5d53
christylau <christy.lau599@gmail.com>
2022-08-10 19:07:06 +0800

    Complete getbooking test

--- cypress/e2e/getBookings.cy.js
+++ cypress/e2e/getBookings.cy.js
@@ -4,14 +4,23 @@ describe('empty spec', () => {
     cy.request("http://localhost:5000/getbookings/testing.out@gmail.com").as('getbook')
 
     cy.get('@getbook').should((response) => {
-      expect((response.body)[0]).to.have.property('firstName')
-      expect((response.body)[0]).to.have.property('lastName')
-      expect((response.body)[0]).to.have.property('phoneNumber')
-      expect((response.body)[0]).to.have.property('emailAddress')
-      expect((response.body)[0]).to.have.property('creditCardNumber')
-      expect((response.body)[0]).to.have.property('cardExpiry')
-      expect((response.body)[0]).to.have.property('CVV_CVC')
-      expect((response.body)[0]).to.have.property('billingAddress')
+
+
+      for(let e = 0; e < response.body.length; e++){
+        expect((response.body)[e]).to.have.property('firstName')
+        expect((response.body)[e]).to.have.property('lastName')
+        expect((response.body)[e]).to.have.property('phoneNumber')
+        expect((response.body)[e]).to.have.property('emailAddress')
+        expect((response.body)[e].emailAddress).to.equal("testing.out@gmail.com")
+        expect((response.body)[e]).to.have.property('creditCardNumber')
+        expect((response.body)[e]).to.have.property('cardExpiry')
+        expect((response.body)[e]).to.have.property('CVV_CVC')
+        expect((response.body)[e]).to.have.property('billingAddress')
+
+      }
+
+
+
       
     })
 
